909f556d67a136bb2eda04362e6cb76c75a796d2
DOROTHY2322 <kimelidorothy@gmail.com>
2023-03-23 12:01:45 +0300

    signup request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+    include ActionController::Cookies
+
+    def app_response(message: 'success', status: 200, data: nil)
+        render json: {
+            message: message,
+            data: data
+        }, status: status
+    end
+    # store user id in session
+    def save_user(id)
+        session[:uid] = id
+        session[:expiry] = 6.hours.from_now
+    end
+
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+    def register
+        user = User.create(user_params)
+        if user.valid?
+            save_user(id: user.id)
+            app_response(message: 'Registration was successful', status: :created, data: user)
+        else
+            app_response(message: 'Something went wrong during registration', status: :unprocessable_entity, data: user.errors)
+        end
+    end
+ private 
+ def user_params
+    params.permit(:username, :email, :password)
+end
+
+end
