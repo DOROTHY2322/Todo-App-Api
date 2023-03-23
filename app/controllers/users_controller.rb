29b6b701c9ec5ce123af48935902d3efc32337b1
DOROTHY2322 <kimelidorothy@gmail.com>
2023-03-23 14:24:28 +0300

    todos controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+    before_action :session_expired?, only: [:check_login_status]
+
     def register
         user = User.create(user_params)
         if user.valid?
@@ -8,9 +11,31 @@ class UsersController < ApplicationController
             app_response(message: 'Something went wrong during registration', status: :unprocessable_entity, data: user.errors)
         end
     end
- private 
- def user_params
-    params.permit(:username, :email, :password)
-end
+
+    def login
+        sql = "username = :username OR email = :email"
+        user = User.where(sql, { username: user_params[:username], email: user_params[:email] }).first
+        if user&.authenticate(user_params[:password])
+            save_user(id: user.id)
+            app_response(message: 'Login was successful', status: :ok, data: user)
+        else
+            app_response(message: 'Invalid username/email or password', status: :unauthorized)
+        end
+    end
+
+    def logout
+        remove_user
+        app_response(message: 'Logout successful', status:ok)
+    end
+
+    def check_login_status
+        app_response(message: 'success', status: :ok)
+    end
+
+    private 
+    
+    def user_params
+        params.permit(:username, :email, :password)
+    end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,14 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module TodoAppApi
+module Api
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+#cookies and sessions
+config.middleware.use ActionDispatch::Cookies
+config.middleware.use ActionDispatch::Session::CookieStore
+config.action_dispatch.cookies_same_site_protection = :strict
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
